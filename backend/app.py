7ed8b77bf2ac410bda41d0f6c8835445762c1256
anishaachaudhuri <heyanishaa.tech@gmail.com>
2026-05-06 01:48:20 +0530

    added pdf text extraction using PyMuPDF

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI, File, UploadFile
 import shutil
 import os
+import fitz
 
 app = FastAPI()
 
 UPLOAD_FOLDER = "data/resumes"
-
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.get("/")
 def home():
     return {"message": "HR System Running"}
+
+def extract_text_from_pdf(file_path):
+    doc = fitz.open(file_path)
+    text = ""
+
+    for page in doc:
+        text += page.get_text()
+    return text
 
 @app.post("/upload")
 def upload_resume(file: UploadFile = File(...)):
@@ -18,5 +26,7 @@ def upload_resume(file: UploadFile = File(...)):
     
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-    
-    return {"filename": file.filename, "status": "uploaded"}
+
+    extracted_text = extract_text_from_pdf(file_path)
+
+    return {"filename": file.filename, "status": "uploaded", "preview": extracted_text[:500]}
