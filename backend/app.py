a153c632b8addecafabf2cfb0d1293db163f021c
anishaachaudhuri <heyanishaa.tech@gmail.com>
2026-05-06 01:21:08 +0530

    added resume upload api

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import shutil
+import os
 
 app = FastAPI()
+
+UPLOAD_FOLDER = "data/resumes"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.get("/")
 def home():
     return {"message": "HR System Running"}
+
+@app.post("/upload")
+def upload_resume(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    return {"filename": file.filename, "status": "uploaded"}
